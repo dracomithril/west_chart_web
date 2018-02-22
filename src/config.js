0822c9e47b7d80c9c8dd712b9d3e0f899b2142bd
Michal Grezel <michal.grezel@grandparade.co.uk>
2018-02-22 15:00:08 +0100

    change to react app api url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const loginPath = '/api/spotify/login_r';
 const refreshTokenPath = '/api/spotify/refresh_token';
 const isProduction = process.env.NODE_ENV === 'production';
 const port = process.env.PORT || 3001;
-const hostname = isProduction ? process.env.API_URL : `http://localhost:${port}`;
+const hostname = isProduction ? process.env.REACT_APP_API_URL : `http://localhost:${port}`;
 const config = {
   isProduction,
   port,
